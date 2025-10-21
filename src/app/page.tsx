881fb8bc7c9596bb99a1b7ec5340947be5d3c1a9
edogarciapanjaitan <edson.bennetgarcia@gmail.com>
2025-10-21 11:20:11 +0700

    company

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-import Footer from "@/Components/Footer";
-import Navbar from "@/Components/Navbar"
 import React from "react"
-import Contact from "./ContactUs/page";
-import AboutUs from "./AboutUs/page";
 import Home from "./Home/page";
-import Events from "./Events/page";
+
 
 export const Company = () => {
   return (
